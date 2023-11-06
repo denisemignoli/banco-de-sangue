0e8838a9e207a7e559b098affafdee31325e6e82
denisemignoli <denisemignoli@gmail.com>
2023-11-05 23:47:47 -0300

    add registration form validation

--- acaoCadastro.php
+++ acaoCadastro.php
@@ -1,54 +1,81 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Doador</title>
-    <link rel="stylesheet" href="./css/cadastro-e-login-style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cadastro de Doador</title>
+  <link rel="stylesheet" href="./css/cadastro-e-login-style.css">
 </head>
+
 <body>
-    <div class="container">
-        <h1 class="alignCenter">Cadastro</h1>
-        <?php
-    
-        session_start();
+  <div class="container">
+    <h1 class="alignCenter">Cadastro</h1>
+    <?php
+    session_start();
 
-        echo "Nome: ".$_POST['nome']."<br>";
-        echo "CPF: ".$_POST['cpf']."<br>";
-        echo "Email: ".$_POST['email']."<br>";
-        echo "Login: ".$_POST['login']."<br>";
-        echo "Senha: ".$_POST['senha']."<br>";
+    $nome = $_POST['nome'];
+    $cpf = $_POST['cpf'];
+    $email = $_POST['email'];
+    $login = $_POST['login'];
+    $senha = $_POST['senha'];
+    $confirmaSenha = $_POST['confirma_senha'];
 
-        require_once 'conexaoBD.php';
-        
-        $sql = "INSERT INTO tb_pessoa (cpf, nome, email, login, senha) 
-        VALUES ('".$_POST['cpf']."', '".$_POST['nome']."', '".$_POST['email']."', '".$_POST['login']."','".$_POST['senha']."')";  
-  
-        if ($conexao->query($sql) === TRUE) {
+    // Validações de preenchimento dos campos.
+    if (empty($nome)) {
+      echo "Nome não pode estar em branco.<br>";
+      exit;
+    }
 
-            echo '
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      echo "Email inválido.<br>";
+      exit;
+    }
+
+    if (empty($login)) {
+      echo "Login não pode estar em branco.<br>";
+      exit;
+    }
+
+    if (strlen($senha) < 8) {
+      echo "A senha deve conter pelo menos 8 caracteres.<br>";
+      exit;
+    }
+
+    if ($senha !== $confirmaSenha) {
+      echo "A confirmação de senha não corresponde à senha fornecida.<br>";
+      exit;
+    }
+
+    require_once 'conexaoBD.php';
+
+    $sql = "INSERT INTO tb_pessoa (cpf, nome, email, login, senha) 
+        VALUES ('" . $_POST['cpf'] . "', '" . $_POST['nome'] . "', '" . $_POST['email'] . "', '" . $_POST['login'] . "','" . $_POST['senha'] . "')";
+
+    if ($conexao->query($sql) === TRUE) {
+
+      echo '
               <div>
               <a href="./principal.php">
               <h1>Seja bem vindo(a)!</h1>
               </a>
               </div>          
             ';
+    } else {
 
-          } else {
-
-            echo '
+      echo '
             <div>
             <a href="./index.php">
             <h1>Login inválido</h1>
             </a>
             </div>          
           ';
+    }
 
-          }
+    $conexao->close();
+    ?>
 
-        $conexao->close();
-        ?>
-
-</div>
+  </div>
 </body>
+
 </html>

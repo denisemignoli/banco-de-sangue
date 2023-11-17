a24236bf86b7e6fd6ab7a908e59b897b7cede9f7
denisemignoli <denisemignoli@gmail.com>
2023-11-17 00:23:53 -0300

    reviewed

--- requisitos.php
+++ requisitos.php
@@ -1,109 +1,105 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
 <head>
-    <title>PHP Test</title>
-    <meta charset="UTF-8"/>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="./css/requisitos.css" />
+  <meta charset="UTF-8">
+  <title>Requisitos</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="./css/requisitos.css">
 </head>
+
 <body>
-    <div class="navbar">
-        <ul>
-          <li><a href="index.php" style="margin-left:40px">Home</a></li>
-          <li><a href="#">Requisitos de doação</a></li>
-          <li><a href="processo.php">Processo de doação</a></li>
-          <li><a href="hemocentros.php">Onde doar?</a></li>
-        </ul>
+
+  <!-- Navbar -->
+  <div class="navbar">
+    <ul>
+      <li><a href="index.php" style="margin-left:40px">Home</a></li>
+      <li><a href="#">Requisitos de doação</a></li>
+      <li><a href="processo.php">Processo de doação</a></li>
+      <li><a href="hemocentros.php">Onde doar?</a></li>
+    </ul>
+  </div>
+
+  <!-- Conteúdo principal -->
+  <div>
+    <div class="info">
+      <h2 style="text-align:center;">Requisitos para doação de sangue</h2>
+
+      <h3>Requisitos básicos</h3>
+      <ul>
+        <li><i class="fa fa-check"></i> Estar em boas condições de saúde.</li>
+        <li><i class="fa fa-check"></i> Ter entre 16 e 69 anos, desde que a primeira doação tenha sido feita até 60 anos.</li>
+        <li><i class="fa fa-check"></i> Pesar no mínimo 50kg.</li>
+        <li><i class="fa fa-check"></i> Estar descansado (ter dormido pelo menos 6 horas nas últimas 24 horas).</li>
+        <li><i class="fa fa-check"></i> Estar alimentado (evitar alimentação gordurosa nas 4 horas que antecedem a doação).</li>
+        <li><i class="fa fa-check"></i> Apresentar documento original com foto recente, que permita a identificação do candidato, emitido por órgão oficial.</li>
+      </ul>
+
+      <h3>Principais impedimentos temporários</h3>
+      <ul>
+        <li><i class="fa fa-exclamation"></i> Resfriado: aguardar 7 dias após desaparecimento dos sintomas.</li>
+        <li><i class="fa fa-exclamation"></i> Gravidez.</li>
+        <li><i class="fa fa-exclamation"></i> 90 dias após parto normal e 180 dias após cesariana.</li>
+        <li><i class="fa fa-exclamation"></i> Amamentação (se o parto ocorreu há menos de 12 meses).</li>
+        <li><i class="fa fa-exclamation"></i> Ingestão de bebida alcoólica nas 12 horas que antecedem a doação.</li>
+        <li><i class="fa fa-exclamation"></i> Tatuagem, maquiagem definitiva e micropigmentação (sobrancelhas, lábios, etc.): aguardar 12 meses.</li>
+        <li><i class="fa fa-exclamation"></i> Situações nas quais há maior risco de adquirir doenças sexualmente transmissíveis: aguardar 6 meses.</li>
+        <li><i class="fa fa-exclamation"></i> Qualquer procedimento endoscópico (endoscopia digestiva alta, colonoscopia, rinoscopia, etc.): aguardar 6 meses.</li>
+        <li><i class="fa fa-exclamation"></i> Extração dentária (verificar uso de medicação) ou tratamento de canal (verificar medicação): por 7 dias.</li>
+        <li><i class="fa fa-exclamation"></i> Cirurgia odontológica com anestesia geral: por 4 semanas.</li>
+        <li><i class="fa fa-exclamation"></i> Acupuntura: com material descartável/agulha própria, aguardar 24 horas; em procedimento não seguro, aguardar 6 meses; se realizada com laser ou sementes: apto.</li>
+        <li><i class="fa fa-exclamation"></i> Vacina contra gripe: por 48 horas.</li>
+        <li><i class="fa fa-exclamation"></i> Vacina contra Coronarivus: 48 horas após cada dose (Coronavac e Covaxin), 7 dias após cada dose (AstraZeneca, Pfizer, Janssen-Cilag e Moderna).</li>
+        <li><i class="fa fa-exclamation"></i> Viagem ao exterior para os Estados Unidos ou regiões com surto de Febre Amarela impede a doação por 30 dias, após o retorno.</li>
+        <li><i class="fa fa-exclamation"></i> Viagem no Brasil: estados como Acre, Amapá, Amazonas, Rondônia, Roraima, Maranhão, Mato Grosso, Pará e Tocantins são locais onde há alta prevalência de malária. Quem esteve nesses estados deve aguardar 12 meses para doar, após o retorno.</li>
+      </ul>
+
+      <h3>Principais impedimentos definitivos</h3>
+      <ul>
+        <li><i class="fa fa-minus"></i> Hepatite após os 11 anos de idade.</li>
+        <li><i class="fa fa-minus"></i> Evidência clínica ou laboratorial das seguintes doenças infecciosas transmissíveis pelo sangue: Hepatites B e C, AIDS (vírus HIV), doenças associadas aos vírus HTLV I e II e Doença de Chagas.</li>
+        <li><i class="fa fa-minus"></i> Uso de drogas ilícitas injetáveis.</li>
+        <li><i class="fa fa-minus"></i> Malária.</li>
+        <li><i class="fa fa-minus"></i> Doença de Parkinson.</li>
+      </ul>
     </div>
 
-    <div class="container">
-        <div class="hero-image">
-          <div class="hero-text">
-            <h1 style="font-size:45px">Salve vidas, doe sangue!</h1>
-              <a href="cadastro.php" style="margin-left:0px" class="button">Seja um doador!</a>
-          </div>
-        </div>
-    </div>
-    
     <div>
-      <div class="info">
-        <h2 style="text-align:center;">Requisitos para doação de sangue</h2>
-
-        <h3>Requisitos básicos</h3>
-        <ul>
-          <li><i class="fa fa-check"></i> Estar em boas condições de saúde.</li>
-          <li><i class="fa fa-check"></i> Ter entre 16 e 69 anos, desde que a primeira doação tenha sido feita até 60 anos.</li>
-          <li><i class="fa fa-check"></i> Pesar no mínimo 50kg.</li>
-          <li><i class="fa fa-check"></i> Estar descansado (ter dormido pelo menos 6 horas nas últimas 24 horas).</li>
-          <li><i class="fa fa-check"></i> Estar alimentado (evitar alimentação gordurosa nas 4 horas que antecedem a doação).</li>
-          <li><i class="fa fa-check"></i> Apresentar documento original com foto recente, que permita a identificação do candidato, emitido por órgão oficial.</li>
-        </ul>
-
-        <h3>Principais impedimentos temporários</h3>
-        <ul>
-          <li><i class="fa fa-exclamation"></i> Resfriado: aguardar 7 dias após desaparecimento dos sintomas.</li>
-          <li><i class="fa fa-exclamation"></i> Gravidez.</li>
-          <li><i class="fa fa-exclamation"></i> 90 dias após parto normal e 180 dias após cesariana.</li>
-          <li><i class="fa fa-exclamation"></i> Amamentação (se o parto ocorreu há menos de 12 meses).</li>
-          <li><i class="fa fa-exclamation"></i> Ingestão de bebida alcoólica nas 12 horas que antecedem a doação.</li>
-          <li><i class="fa fa-exclamation"></i> Tatuagem, maquiagem definitiva e micropigmentação (sobrancelhas, lábios, etc.): aguardar 12 meses.</li>
-          <li><i class="fa fa-exclamation"></i> Situações nas quais há maior risco de adquirir doenças sexualmente transmissíveis: aguardar 6 meses.</li>
-          <li><i class="fa fa-exclamation"></i> Qualquer procedimento endoscópico (endoscopia digestiva alta, colonoscopia, rinoscopia, etc.): aguardar 6 meses.</li>
-          <li><i class="fa fa-exclamation"></i> Extração dentária (verificar uso de medicação) ou tratamento de canal (verificar medicação): por 7 dias.</li>
-          <li><i class="fa fa-exclamation"></i> Cirurgia odontológica com anestesia geral: por 4 semanas.</li>
-          <li><i class="fa fa-exclamation"></i> Acupuntura: com material descartável/agulha própria, aguardar 24 horas; em procedimento não seguro, aguardar 6 meses; se realizada com laser ou sementes: apto.</li>
-          <li><i class="fa fa-exclamation"></i> Vacina contra gripe: por 48 horas.</li>
-          <li><i class="fa fa-exclamation"></i> Vacina contra Coronarivus: 48 horas após cada dose (Coronavac e Covaxin), 7 dias após cada dose (AstraZeneca, Pfizer, Janssen-Cilag e Moderna).</li>
-          <li><i class="fa fa-exclamation"></i> Viagem ao exterior para os Estados Unidos ou regiões com surto de Febre Amarela impede a doação por 30 dias, após o retorno.</li>
-          <li><i class="fa fa-exclamation"></i> Viagem no Brasil: estados como Acre, Amapá, Amazonas, Rondônia, Roraima, Maranhão, Mato Grosso, Pará e Tocantins são locais onde há alta prevalência de malária. Quem esteve nesses estados deve aguardar 12 meses para doar, após o retorno.</li>
-        </ul>
-
-        <h3>Principais impedimentos definitivos</h3>
-        <ul>
-          <li><i class="fa fa-minus"></i> Hepatite após os 11 anos de idade.</li>
-          <li><i class="fa fa-minus"></i> Evidência clínica ou laboratorial das seguintes doenças infecciosas transmissíveis pelo sangue: Hepatites B e C, AIDS (vírus HIV), doenças associadas aos vírus HTLV I e II e Doença de Chagas.</li>
-          <li><i class="fa fa-minus"></i> Uso de drogas ilícitas injetáveis.</li>
-          <li><i class="fa fa-minus"></i> Malária.</li>
-          <li><i class="fa fa-minus"></i> Doença de Parkinson.</li>
-        </ul>
-      </div>
-      
-      <div>
-        <h3>Respeitar os intervalos para doação:</h3>
-        <ul>
-          <li><span>Homens: </span>60 dias (máximo de 04 doações nos últimos 12 meses).</li>
-          <li><span>Mulheres: </span>90 dias (máximo de 03 doações nos últimos 12 meses).</li>
-        </ul>
-      </div>
+      <h3>Respeitar os intervalos para doação:</h3>
+      <ul>
+        <li><span>Homens: </span>60 dias (máximo de 04 doações nos últimos 12 meses).</li>
+        <li><span>Mulheres: </span>90 dias (máximo de 03 doações nos últimos 12 meses).</li>
+      </ul>
     </div>
-    <br>
-    <hr>
+  </div>
+  <br>
+  <hr>
 
-        <div class="div-container">
-            <div class="div1">
-                <h2>Cadastro de Doador</h2>
-                <p>Novo por aqui? Faça o seu cadastro.</p>
-                <a href="cadastro.php" class="button">Cadastrar</a>
-            </div>
-            <div class="div2">
-                <h2>Login de Doador</h2>
-                <p>Já é um doador? Faça o login.</p>
-                <a href="login.php" class="button">Login</a>
-            </div>
-        </div>
+  <!-- Div de cadastro/login -->
+  <div class="div-container">
+    <div class="div1">
+      <h2>Cadastro de Doador</h2>
+      <p>Novo por aqui? Faça o seu cadastro.</p>
+      <a href="cadastro.php" class="button">Cadastrar</a>
     </div>
-    <br>
-    <hr>
-    <div>
-        <footer>
-            <img src="./assets/facebook.png" alt="facebook icon" width="30">
-            <img src="./assets/twitter.png" alt="twitter icon" width="30">
-             <img src="./assets/youtube.png" alt="youtube icon" width="30">
-            <img src="./assets/instagram.png" alt="instagram icon" width="30">
-        </footer>
+    <div class="div2">
+      <h2>Login de Doador</h2>
+      <p>Já é um doador? Faça o login.</p>
+      <a href="login.php" class="button">Login</a>
     </div>
-    <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right">      </script>
+  </div>
+  </div>
+  <br>
+  <hr>
+  <div>
+    <footer>
+      <img src="./assets/facebook.png" alt="facebook icon" width="30">
+      <img src="./assets/twitter.png" alt="twitter icon" width="30">
+      <img src="./assets/youtube.png" alt="youtube icon" width="30">
+      <img src="./assets/instagram.png" alt="instagram icon" width="30">
+    </footer>
+  </div>
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"> </script>
 </body>
+
 </html>

17d71418a10a2541fcb0f7538676f855b5ed6a70
denisemignoli <denisemignoli@gmail.com>
2023-11-20 17:48:10 -0300

    add review

--- doador.php
+++ doador.php
@@ -1,13 +1,13 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-  <title>PHP Test</title>
-  <meta charset="UTF-8"/>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./css/doador.css">
-  <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>Doador</title>
 </head>
+
 <body>
 
   <div class="navbar">
@@ -19,150 +19,152 @@
     </ul>
   </div>
 
-    <div class="container">
-        <div class="hero-image">
-          <div class="hero-text">
-            <h1 style="font-size:45px">Bem-vindo a Área do Doador!</h1>
-            <p>Doe sangue, doe vida.</p>
-          </div>
+  <div class="container">
+    <!-- DADOS PESSOAIS --->
+    <div class="column">
+      <div class="personal-info-texto">
+        <div>
+          <h3>Dados pessoais</h3>
         </div>
+        <form>
+          <?php
+          session_start();
+
+          $nome = $cpf = $email = $dataNascimento = $login = $cidade = $telefone = $tipoSanguineo = "";
+
+          // Verifica se o CPF está na sessão
+          if (isset($_SESSION['cpf'])) {
+            $cpfDoUsuario = $_SESSION['cpf'];
+
+            // Consulta SQL com o CPF do usuário logado
+            $sql = "SELECT *
+            FROM tb_pessoa
+            INNER JOIN tb_doador ON tb_pessoa.cpf = tb_doador.cpf
+            WHERE tb_pessoa.cpf = '$cpfDoUsuario'";
+
+            require './conexaoBD.php';
+            $resultado = $conexao->query($sql);
+            if ($resultado != null) {
+              while ($row = $resultado->fetch_assoc()) {
+                // informacoes tb_pessoa
+                $nome = $row["nome"];
+                $cpf = $row["cpf"];
+                $email = $row["email"];
+                $login = $row["login"];
+                // informacoes tb_doador
+                $dataNascimento = $row["data_nascimento"];
+                $sexo = $row["sexo"];
+                $logradouro = $row["logradouro"];
+                $bairro = $row["bairro"];
+                $cidade = $row["cidade"];
+                $telefone = $row["telefone"];
+                $tipoSanguineo = $row["tipo_sanguineo"];
+              }
+            }
+            $conexao->close();
+          }
+          ?>
+
+          <div class="image-and-info">
+            <div class="image">
+              <img src="./assets/profile.svg" alt="" style="width: 250px;">
+            </div>
+            <div class="info">
+              <!-- Informações do doador -->
+              <p id="nome"><span>Nome: </span><?php echo $nome; ?></p>
+              <p id="cpf"><span>CPF: </span><?php echo $cpf; ?></p>
+              <p id="email"><span>Email: </span><?php echo $email; ?></p>
+              <p id="dataNascimento"><span>Data de Nascimento: </span><?php echo $dataNascimento; ?></p>
+              <p id="sexo"><span>Sexo: </span><?php echo $sexo; ?></p>
+              <p id="endereco"><span>Endereço: </span><?php echo $logradouro; ?></p>
+              <p id="cidade"><span>Cidade: </span><?php echo $cidade; ?></p>
+              <p id="telefone"><span>Telefone: </span><?php echo $telefone; ?></p>
+              <p id="tipoSanguineo"><span>Tipo sanguíneo: </span><?php echo $tipoSanguineo; ?></p>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
 
-    <div class="info">
-        <section class="personal-info"> 
-            <h2>Dados Pessoais</h2>
-            <div class="personal-info-items">
-              <img src="./assets/profile.svg" alt="">
-              <div class="personal-info-texto">
-                <form>
-                  <?php 
-                    require './conexaoBD.php';
-                    $sql = "SELECT *
-                    FROM tb_pessoa
-                    INNER JOIN tb_doador ON tb_pessoa.cpf = tb_doador.cpf
-                    WHERE tb_pessoa.cpf = '12345678901';
-                    ";
-                    $resultado = $conexao->query($sql);
-                    if ($resultado != null) {
-                      while ($row = $resultado->fetch_assoc()) {
-                        echo "<p><span style='font-weight: bold; color: #960000;'>Nome: </span>" . $row["nome"]. "</p>";
-                        echo "<p><span style='font-weight: bold; color: #960000;'>CPF: </span>" . $row["cpf"]. "</p>";
-                        echo "<p><span style='font-weight: bold; color: #960000;'>Email: </span>" . $row["email"]. "</p>";
-                        echo "<p><span style='font-weight: bold; color: #960000;'>Data de Nascimento: </span>" . $row["data_nascimento"]. "</p>";
-                        echo "<p><span style='font-weight: bold; color: #960000;'>Login: </span>" . $row["login"]. "</p>";
-                        echo "<p><span style='font-weight: bold; color: #960000;'>Cidade: </span>" . $row["cidade"]. "</p>";
-                        echo "<p><span style='font-weight: bold; color: #960000;'>Telefone: </span>" . $row["telefone"]. "</p>";
-                        echo "<p><span style='font-weight: bold; color: #960000;'>Tipo sanguíneo: </span>" . $row["tipo_sanguineo"]. "</p>";
-                    }                   
-                    }
-                    $conexao->close();
-                  ?>
-                </form>
-                <button type="submit" class="button">Alterar informações</button>
-              </div>
-              <div class="formVerificar">
-                <h3>Você está apto para doar?</h3>
-                <form class="">
-                  <div class="form-item">
-                  <label for="peso">Peso: </label>
-                  <input type="text" name="peso">
-                  </div>
-                  <div class="form-item">
-                  <label for="altura">Altura: </label>
-                  <input type="text" name="altura">
-                  </div>
-                  <div class="form-item">
-                  <label for="altura">Idade: </label>
-                  <input type="text" name="idade"><br>
-                  </div>
-                  <button type="button" class="button" onclick="verificar()" style="width: 250px;">Verificar</button>
-                <script>
-                  function verificar() {
-                      // Obtém os valores do formulário
-                      var peso = parseFloat(document.getElementsByName("peso")[0].value);
-                      var altura = parseFloat(document.getElementsByName("altura")[0].value);
-                      var idade = parseInt(document.getElementsByName("idade")[0].value);
+    <!-- VERIFICACAO --->
+    <div class="column">
+      <div class="formVerificar">
+        <h3>Você está apto para doar?</h3>
+        <form>
+          <div class="form-item">
+            <label for="peso">Peso: </label>
+            <input type="number" name="peso">
+          </div>
+          <div class="form-item">
+            <label for="altura">Altura: </label>
+            <input type="number" name="altura">
+          </div>
+          <div class="form-item">
+            <label for="idade">Idade: </label>
+            <input type="number" name="idade"><br>
+          </div>
+          <button type="button" class="button" onclick="verificar()" style="width: 250px;">Verificar</button>
+        </form>
+        <div id="resultado"></div>
+      </div>
+      <script>
+        function verificar() {
+          // Obtém os valores do formulário
+          var peso = parseFloat(document.getElementsByName("peso")[0].value);
+          var altura = parseFloat(document.getElementsByName("altura")[0].value);
+          var idade = parseInt(document.getElementsByName("idade")[0].value);
 
-                      // Converte altura para metros (se estiver em centímetros)
-                      if (altura > 3) {
-                          altura /= 100; // Converte para metros
-                      }
+          // Converte altura para metros (se estiver em centímetros)
+          if (altura > 3) {
+            altura /= 100; // Converte para metros
+          }
 
-                      // Verifica se atende aos critérios para doação de sangue
-                      if (idade >= 18 && idade <= 65 && peso >= 50 && altura >= 1.50) {
-                          document.getElementById("resultado").innerHTML = "<h3>Você está apto para doar sangue!</h3>";
-                      } else {
-                          document.getElementById("resultado").innerHTML = "<h3>Você não atende aos critérios para doação de sangue.</h3>" +
-                              "<p>Por favor, verifique os critérios <a href='requisitos.php'>aqui</a>!</p>";
-                      }
-                  }
-                </script>
-                </form>
-              </div>             
-              <div id="resultado"></div>
-              </div>
-        </section>
-
-        <section>
-            <h2>Histórico de Doações</h2>
-            <table class="tabela-historico">
-              <thead>
-                <tr>
-                  <th>Data</th>
-                  <th>Local</th>
-                  <th>Observações</th>
-                </tr>
-              <thead>
-              <?php 
-                require './conexaoBD.php';
-                $sql = 'SELECT *
-                FROM tb_doador
-                INNER JOIN tb_historico_doacoes 
-                ON tb_historico_doacoes.id_doador = tb_doador.id_doador
-                WHERE tb_historico_doacoes.id_doador = 1;';
-                $resultado = $conexao->query($sql);
-                if ($resultado != null) {
-                  while ($row = $resultado->fetch_assoc()) {
-                    echo '<tr>';
-                    echo '<td>' . $row["data_doacao"]. "</td>";
-                    echo '<td>' . $row["local_doacao"]. "</td>";
-                    echo '<td>' . $row["ds_observacoes"]. "</td>";
-                    echo '</tr>';
-                }                   
-                }
-                $conexao->close();
-              ?>
-            </table>
-        </section>
-
-        <!-- <section class="test-results">
-            <h2>Resultados de Exames</h2>
-            <p>...</p>
-        </section> -->
-
-        <section class="future-donations">
-            <h2>Agendamento de Doações Futuras</h2>
-            <form>
-                <label for="data_agendamento">Data de Agendamento:</label>
-                <input type="date" id="data_agendamento" name="data_agendamento" required><br>
-                <button type="submit" class="button" style="margin-top: 1rem;">Agendar Doação</button>
-            </form> 
-        </section>
-
-        <!-- <section class="communications">
-            <h2>Comunicações e Notificações</h2>
-            <p>...</p>
-        </section> -->
-
-        <!-- <section class="privacy-settings">
-            <h2>Configurações de Privacidade</h2>
-            <p>...</p>
-        </section> -->
-
-        <!-- <section class="emergency-info">
-            <h2>Informações de Emergência</h2>
-            <p>Adicione aqui o código para exibir informações de emergência do doador</p>
-        </section> -->
+          // Verifica se atende aos critérios para doação de sangue
+          if (idade >= 18 && idade <= 65 && peso >= 50 && altura >= 1.50) {
+            document.getElementById("resultado").innerHTML = "<h3>Você está apto para doar sangue!</h3>";
+          } else {
+            document.getElementById("resultado").innerHTML = "<h3>Você não atende aos critérios para doação de sangue.</h3>" +
+              "<p>Por favor, verifique os critérios <a href='requisitos.php'>aqui</a>!</p>";
+          }
+        }
+      </script>
     </div>
+  </div>
+
+  <!-- HISTORICO DE DOACOES --->
+  <section>
+    <h3>Histórico de Doações</h3>
+    <table class="tabela-historico">
+      <thead>
+        <tr>
+          <th>Data</th>
+          <th>Local</th>
+          <th>Observações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        require './conexaoBD.php';
+        $sql = 'SELECT *
+                        FROM tb_doador
+                        INNER JOIN tb_doacao 
+                        ON tb_doacao.id_doador = tb_doador.id_doador
+                        WHERE tb_doacao.id_doador = 1;';
+        $resultado = $conexao->query($sql);
+        if ($resultado != null) {
+          while ($row = $resultado->fetch_assoc()) {
+            echo '<tr>';
+            echo '<td>' . $row["data_doacao"] . "</td>";
+            echo '<td>' . $row["local_doacao"] . "</td>";
+            echo '<td>' . $row["ds_observacoes"] . "</td>";
+            echo '</tr>';
+          }
+        }
+        $conexao->close();
+        ?>
+      </tbody>
+    </table>
+  </section>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -1,18 +1,30 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Doador</title>
     <link rel="stylesheet" href="./css/cadastro-e-login-style.css">
 </head>
+
 <body>
     <div class="container">
         <h1 class="alignCenter">Entrar</h1>
         <div class="alignLeft">
             <form id="form1" name="form1" method="post" action="acaoLogin.php">
-                <label for="login">Login:</label>
-                <input name="login" type="text" id="login" placeholder="Digite seu login" required>
+                <label for="cpf">CPF:</label>
+                <input name="cpf" type="text" id="cpf" placeholder="Digite seu cpf" required>
+                <script>
+                    function formatCPF() {
+                        const cpfInput = document.getElementById("cpf");
+                        const cpfValue = cpfInput.value.replace(/\D/g, '');
+                        const formattedCPF = cpfValue.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+                        cpfInput.value = formattedCPF;
+                    }
+                    const cpfInput = document.getElementById("cpf");
+                    cpfInput.addEventListener("input", formatCPF);
+                </script>
                 <label for="senha">Senha:</label>
                 <input name="senha" type="password" id="senha" placeholder="Digite sua senha" required>
                 <div class="button-container">
@@ -23,4 +35,5 @@
         </div>
     </div>
 </body>
+
 </html>
